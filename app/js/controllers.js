137fcbff8d4ee59692db99dd3744719ca01bb523
Fredrik Antell <fred@fredantell.com>
2014-08-18 10:43:25 -0500

    Setup scaffolding and static html.  Create data service and basic controller.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,7 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
+        this.love = "burgers and fries.";
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
